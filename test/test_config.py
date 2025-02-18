8b86a806c637308ce75eec843090dca3bb590787
Juan GVR <juan@multilayer.io>
2025-02-18 13:42:06 +0100

    more docs, a few tests

--- test/test_config.py
+++ test/test_config.py
@@ -1,14 +1,14 @@
 import os
 import pytest
 
-from jupancon.config import JPTConfig
+from jupancon.config import JPConfig
 
 
 config_path = f"{os.path.abspath(os.path.dirname(__file__))}/test_configfile.yml"
 
 def test_jptconfig_default():
     print(config_path)
-    config = JPTConfig(configfile=config_path)
+    config = JPConfig(configfile=config_path)
     assert config.name == 'totally-real-gcp'
     assert config.dbtype == 'bigquery'
     
@@ -16,4 +16,4 @@ def test_jptconfig_default():
 
 def test_change_non_existing():
     with pytest.raises(Exception) as execinfo:
-        JPTConfig(configfile=config_path).change("totally-exists")
+        JPConfig(configfile=config_path).change("totally-exists")
